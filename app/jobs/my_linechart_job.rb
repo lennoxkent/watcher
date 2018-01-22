a4c46efc1f03f002c9ad34000a489dfe9a61cf91
Michael Kent <michael.kent@Michael-Kent-3245.local>
2018-01-22 07:58:58 +0200

    add get method for 3rd party api in job

--- app/jobs/my_linechart_job.rb
+++ app/jobs/my_linechart_job.rb
@@ -1,8 +1,19 @@
-source = 'http://some.remote.host/linechart.xml'
+require 'net/http'
+require 'uri'
+require 'json'
+
+SOURCE_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo'
 
 labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
 
 Dashing.scheduler.every '2s' do
+
+  url = URI.parse(SOURCE_URL)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = (url.scheme == 'https')
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  response = http.request(Net::HTTP::Get.new(url.request_uri))
+
 
   data = [
       {
@@ -15,6 +26,12 @@ Dashing.scheduler.every '2s' do
           label: 'Second dataset',
           data: Array.new(labels.length) { rand(40..80) },
           backgroundColor: [ 'rgba(255, 206, 86, 0.2)' ] * labels.length,
+          borderColor: [ 'rgba(255, 206, 86, 1)' ] * labels.length,
+          borderWidth: 1,
+      }, {
+          label: 'Third dataset',
+          data: Array.new(labels.length) { rand(40..80) },
+          backgroundColor: [ 'rgba(255, 206, 0, 0.2)' ] * labels.length,
           borderColor: [ 'rgba(255, 206, 86, 1)' ] * labels.length,
           borderWidth: 1,
       }
